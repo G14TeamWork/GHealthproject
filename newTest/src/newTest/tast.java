7510f5f9c707a4fa9640b5302095ad17e6904652
ron <ron@MSI>
2016-05-18 10:55:42 +0300

    commiting

--- newTest/src/newTest/tast.java
+++ newTest/src/newTest/tast.java
@@ -5,7 +5,8 @@ public class tast {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-
+		
+		System.out.println("hello world2");
 	}
 
 }
